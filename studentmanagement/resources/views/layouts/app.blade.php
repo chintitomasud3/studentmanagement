a1296c66ded6b6ef66630b26c8a65821dba46805
chintitomasud3 <mrahman021992@gmail.com>
2023-02-12 13:07:54 +0600

    teacheradded

--- studentmanagement/resources/views/layouts/app.blade.php
+++ studentmanagement/resources/views/layouts/app.blade.php
@@ -57,7 +57,10 @@
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{url('/course')}}">Course</a>
                                     </li>
-                                    <li class="nav-item dropdown">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{url('/teacher')}}">Teachers</a>
+                                    </li>
+                                    <!-- <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             Dropdown
                                         </a>
@@ -70,12 +73,12 @@
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link disabled" href="#">Disabled</a>
-                                    </li>
+                                    </li> -->
                                 </ul>
-                                <form class="form-inline my-2 my-lg-0">
+                                <!-- <form class="form-inline my-2 my-lg-0">
                                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                                </form>
+                                </form> -->
                             </div>
                         </nav>
                     </ul>
